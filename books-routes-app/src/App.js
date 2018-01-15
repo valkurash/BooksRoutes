f7e40f848ad35bf016189ea3f2faf3972db91431
valkurash <valkurash@mail.com>
2018-01-15 20:00:46 +0300

    add react project

---
+++ books-routes-app/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './images/ib-logo.png';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Books routes</h1>
+        </header>
+        <div className="App-content">
+          Content
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
