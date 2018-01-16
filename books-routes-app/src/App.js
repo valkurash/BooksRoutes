bd8f160bd89054c894732cdc0ce702e3e3cdeeba
valkurash <valkurash@mail.com>
2018-01-16 09:45:47 +0300

    header style

--- books-routes-app/src/App.js
+++ books-routes-app/src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Books routes</h1>
+          <h1 className="App-title">Books<br/>Routes</h1>
         </header>
         <div className="App-content">
           Content
