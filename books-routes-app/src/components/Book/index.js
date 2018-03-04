64e4da99867dad9b3350ab6e0fdc7b8f24863f7b
valkurash <valkurash@gmail.com>
2018-03-04 20:44:00 +0300

    add routes to single book item

--- books-routes-app/src/components/Book/index.js
+++ books-routes-app/src/components/Book/index.js
@@ -1,40 +1,46 @@
-import React, {Component} from 'react';
-import PropTypes from 'prop-types';
-import './style.css';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import "./style.css";
 
 export default class Book extends Component {
-    static propTypes = {
-        book: PropTypes.shape({
-            id: PropTypes.number.isRequired,
-            isbn: PropTypes.string,
-            title: PropTypes.string.isRequired,
-            cover_url: PropTypes.string,
-            description: PropTypes.string,
-            authors: PropTypes.arrayOf(PropTypes.shape({id: PropTypes.number.isRequired, avatar: PropTypes.string.isRequired, name: PropTypes.string})),
-            genres: PropTypes.arrayOf(PropTypes.shape({id: PropTypes.number.isRequired, title: PropTypes.string.isRequired}))
+  static propTypes = {
+    book: PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      isbn: PropTypes.string,
+      title: PropTypes.string.isRequired,
+      cover_url: PropTypes.string,
+      description: PropTypes.string,
+      authors: PropTypes.arrayOf(
+        PropTypes.shape({
+          id: PropTypes.number.isRequired,
+          avatar: PropTypes.string.isRequired,
+          name: PropTypes.string
         })
-    }
+      )
+    })
+  };
 
-    render() {
-        const {book} = this.props;
+  render() {
+    const { book } = this.props;
 
-        const authors = book
-            .authors
-            .map((author) => author.name)
-            .join(', ');
+    const authors = book.authors.map(author => author.name).join(", ");
 
-        if (!book) 
-            return null;
-        
-        return (
-            <div className="book-item">
-                <div className="cover"><img alt="book cover" src={book.cover}/></div>
-                <div className="info">
-                    <div className="title">{book.title}</div>
-                    <div className="authors">{authors}</div>
-                </div>
-                <div className="description">{book.description}</div>
-            </div>
-        );
-    }
+    if (!book) return null;
+
+    return (
+      <div className="book-item">
+        <div className="cover">
+          <img alt="book cover" src={book.cover} />
+        </div>
+        <div className="info">
+          <Link to={`/books/${book.id}`}>
+            <div className="title">{book.title}</div>
+          </Link>
+          <div className="authors">{authors}</div>
+        </div>
+        <div className="description">{book.description}</div>
+      </div>
+    );
+  }
 }
--- books-routes-app/src/components/pages/BookRoutesPage.js
+++ books-routes-app/src/components/pages/BookRoutesPage.js
@@ -1,16 +1,23 @@
-import React, {Component} from 'react';
-// import PropTypes from 'prop-types'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
 
 export default class BookRoutesPage extends Component {
-   /* static propTypes = {
+  static propTypes = {
+    match: PropTypes.shape({
+      params: PropTypes.shape({
+        id: PropTypes.node
+      }).isRequired
+    }).isRequired
+  };
 
-    };*/
-
-    render() {
-        return (
-            <div>
-                <h1>BookRoutesPage</h1>
-            </div>
-        );
-    }
+  render() {
+    const { match } = this.props;
+    return (
+      <div>
+        <h1>BookRoutesPage {match.params.id}</h1>
+        <Link to="/">Back to all Books</Link>
+      </div>
+    );
+  }
 }
