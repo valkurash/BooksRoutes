29d4935d0584dbd7368274938f97ab0177418524
valkurash <valkurash@gmail.com>
2018-03-13 23:59:18 +0300

    api link

--- books-routes-app/src/actions/booksActions.js
+++ books-routes-app/src/actions/booksActions.js
@@ -1,16 +1,17 @@
 import * as actionTypes from "../constants/ActionTypes";
 
+const api = "https://booksroutes-api.azurewebsites.net/api/v1/books";
 export function fetchBooks() {
   return {
     type: actionTypes.FETCH_BOOKS,
-    callAPI: "http://localhost:1337/api/v1/books"
+    callAPI: api
   };
 }
 export function fetchBook(id) {
   return {
     type: actionTypes.FETCH_BOOK,
     payload: { id },
-    callAPI: `http://localhost:1337/api/v1/books/${id}`
+    callAPI: `${api}/${id}`
   };
 }
 export const searchTermChanged = searchTerm => {
