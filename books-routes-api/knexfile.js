961207559270413a06cd6ed9ea342a389c062a1c
valkurash <valkurash@gmail.com>
2018-03-15 20:30:33 +0300

    pg tcpKeepAlive=true

--- books-routes-api/knexfile.js
+++ books-routes-api/knexfile.js
@@ -1,30 +1,34 @@
-const path = require('path');
+const path = require("path");
 
-const BASE_PATH = path.join(__dirname, 'src', 'server', 'db');
+const BASE_PATH = path.join(__dirname, "src", "server", "db");
 
 module.exports = {
   test: {
-    client: 'pg',
-    connection: 'postgres://valkurash@booksroutes:Val21postgres@booksroutes.postgres.database.azure.com:5432/books_routes_test?ssl=true',
+    client: "pg",
+    connection:
+      "postgres://valkurash@booksroutes:Val21postgres@booksroutes.postgres.database.azure.com:5432/books_routes_test?ssl=true&tcpKeepAlive=true",
     migrations: {
-      directory: path.join(BASE_PATH, 'migrations')
+      directory: path.join(BASE_PATH, "migrations")
     },
     seeds: {
-      directory: path.join(BASE_PATH, 'seeds')
-    },pool: {
+      directory: path.join(BASE_PATH, "seeds")
+    },
+    pool: {
       min: 2,
       max: 10
     }
   },
- development: {
-    client: 'pg',
-    connection: 'postgres://valkurash@booksroutes:Val21postgres@booksroutes.postgres.database.azure.com:5432/books_routes?ssl=true',
+  development: {
+    client: "pg",
+    connection:
+      "postgres://valkurash@booksroutes:Val21postgres@booksroutes.postgres.database.azure.com:5432/books_routes?ssl=true&tcpKeepAlive=true",
     migrations: {
-      directory: path.join(BASE_PATH, 'migrations')
+      directory: path.join(BASE_PATH, "migrations")
     },
     seeds: {
-      directory: path.join(BASE_PATH, 'seeds')
-    },pool: {
+      directory: path.join(BASE_PATH, "seeds")
+    },
+    pool: {
       min: 2,
       max: 10
     }
