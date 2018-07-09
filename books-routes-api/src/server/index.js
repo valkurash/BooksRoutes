3b52cc3ba69a3bd10286535d3f36d65bcfcaa4ea
valkurash <valkurash@gmail.com>
2018-07-09 16:04:03 +0300

    add prerender

--- books-routes-api/src/server/index.js
+++ books-routes-api/src/server/index.js
@@ -1,20 +1,31 @@
-const Koa = require('koa');
-const bodyParser = require('koa-bodyparser');
-const cors = require('@koa/cors');
+const Koa = require("koa");
+const bodyParser = require("koa-bodyparser");
+const cors = require("@koa/cors");
+const prerender = require("koa-prerender");
 
-const indexRoutes = require('./routes/index');
-const booksRoutes = require('./routes/books');
+// Options
+const options = {
+  //prerender: PRERENDER_SERVER_URL, // optional, default:'http://service.prerender.io/'
+  protocol: "http", // optional, default: this.protocol
+  host: "booksroutes.info", // optional, default: this.host,
+  prerenderToken: "OG6U91hdKwZLIKicK00e" // optional or process.env.PRERENDER_TOKEN
+};
+
+const indexRoutes = require("./routes/index");
+const booksRoutes = require("./routes/books");
 
 const app = new Koa();
 const PORT = process.env.PORT || 1337;
 
 app.use(bodyParser());
 
-const options = {
-  origin: '*',
-};
+app.use(prerender(options));
 
-app.use(cors(options));
+app.use(
+  cors({
+    origin: "*"
+  })
+);
 app.use(indexRoutes.routes());
 app.use(booksRoutes.routes());
 
