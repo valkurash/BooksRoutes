6eb561307b31d4a42f25dd9610aff5cda0287620
valkurash <valkurash@gmail.com>
2018-07-10 00:06:17 +0300

    add iis prerender rule

--- books-routes-api/src/server/index.js
+++ books-routes-api/src/server/index.js
@@ -1,15 +1,6 @@
 const Koa = require("koa");
 const bodyParser = require("koa-bodyparser");
 const cors = require("@koa/cors");
-const prerender = require("koa-prerender");
-
-// Options
-const options = {
-  //prerender: PRERENDER_SERVER_URL, // optional, default:'http://service.prerender.io/'
-  protocol: "http", // optional, default: this.protocol
-  host: "booksroutes.info", // optional, default: this.host,
-  prerenderToken: "OG6U91hdKwZLIKicK00e" // optional or process.env.PRERENDER_TOKEN
-};
 
 const indexRoutes = require("./routes/index");
 const booksRoutes = require("./routes/books");
@@ -18,8 +9,6 @@ const app = new Koa();
 const PORT = process.env.PORT || 1337;
 
 app.use(bodyParser());
-
-//app.use(prerender(options));
 
 app.use(
   cors({
