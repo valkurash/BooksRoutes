86d501ec46ab6eb4906b3fe6048ff78fd92681a9
valkurash <valkurash@gmail.com>
2018-07-25 19:38:32 +0300

    remove custom createGenerateClassName

--- books-routes-app/src/App.js
+++ books-routes-app/src/App.js
@@ -11,7 +11,7 @@ import {
   jssPreset
 } from "@material-ui/core/styles";
 import JssProvider from "react-jss/lib/JssProvider";
-import { create, SheetsRegistry } from "jss";
+import { create } from "jss";
 import ReactGA from "react-ga";
 import "./index.css";
 
@@ -33,37 +33,6 @@ const theme = createMuiTheme({
     }
   }
 });
-const createGenerateClassName1 = () => {
-  const maxRules = 1e10;
-  const env = process.env.NODE_ENV;
-  let ruleCounter = 0;
-  const defaultPrefix = env === "production" ? "c" : "";
-
-  return (rule, sheet) => {
-    ruleCounter += 1;
-
-    if (ruleCounter > maxRules) {
-      console.log(
-        `[JSS] You might have a memory leak. Rule counter is at ${ruleCounter}.`
-      );
-    }
-
-    let prefix = defaultPrefix;
-    let jssId = "";
-
-    if (sheet) {
-      prefix =
-        sheet.options.classNamePrefix || sheet.options.meta || defaultPrefix;
-      if (sheet.options.jss.id != null) jssId += sheet.options.jss.id;
-    }
-
-    if (env === "production") {
-      return `${prefix}${jssId}${ruleCounter}`;
-    }
-
-    return `${prefix + rule.key}-${jssId && `-${jssId}`}-${ruleCounter}`;
-  };
-};
 
 export default class App extends Component {
   render() {
