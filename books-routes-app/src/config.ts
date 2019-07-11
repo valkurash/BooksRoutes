bff016d22d8c97b0b1791028368ea6b22242e24e
Dima <dimaivanov1@mail.ru>
2019-07-11 18:24:12 +0300

    fix  api url

--- books-routes-app/src/config.ts
+++ books-routes-app/src/config.ts
@@ -1,5 +1,5 @@
 export const appName = 'booksroutes';
 export const api =
   process.env.NODE_ENV === 'production'
-    ? 'https://booksroutes-api.azurewebsites.net/api'
+    ? 'http://84.201.158.83:1337/api'
     : 'http://localhost:1337/api';
